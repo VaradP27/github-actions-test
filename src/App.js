faf9121d5f8460a2888fe0891a3b3531e0f45e64
Varad Patil <varad.patil@hackerrank.com>
2022-02-08 21:32:01 +0530

    branch test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Nothing
+          Learn Nothing1
         </a>
       </header>
     </div>
