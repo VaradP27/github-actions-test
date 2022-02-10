dbb7202b600e9f69e36d85ec093306dcba42bbf5
Varad Patil <varad.patil@hackerrank.com>
2022-02-10 22:47:13 +0530

    VvV

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Nothing1
+          Learn React
         </a>
         <a>abc</a>
       </header>
